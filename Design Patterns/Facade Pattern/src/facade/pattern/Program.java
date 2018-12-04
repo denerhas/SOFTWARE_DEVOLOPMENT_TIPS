54d21336f367326ff956bf0cf012f796895ee696
denerhas <45282777+denerhas@users.noreply.github.com>
2018-12-04 21:34:29 +0300

    Add files via upload
    Facade Pattern has been added

---
+++ Design Patterns/Facade Pattern/src/facade/pattern/Program.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade.pattern;
+
+/**
+ *
+ * @author Hasan
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ShapeMaker shapeMaker = new ShapeMaker();
+        
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+    
+}
